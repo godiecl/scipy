f365b876d56727a2a0e35821aae081c3603a6fec
Diego Urrutia-Astorga <mika.cl@gmail.com>
2025-07-03 13:55:07 -0400

    feat: enhance benchmarking functionality with human-readable elapsed time and add humanize package

--- libs/benchmark.py
+++ libs/benchmark.py
@@ -1,9 +1,12 @@
 #  Copyright (c) 2025. Departamento de Ingenieria de Sistemas y Computacion.
+import datetime
+import logging
 import time
 from contextlib import contextmanager
 from logging import Logger
 from typing import Generator, Optional
 
+import humanize
 from typeguard import typechecked
 
 
@@ -13,14 +16,33 @@ def benchmark(
     operation_name: Optional[str] = None,
     log: Optional[Logger] = None,
 ) -> Generator[None, None, None]:
-    """
-    Function to benchmark a function
-    """
-    start: float = time.perf_counter()
+    """Function to benchmark a function."""
+
+    # time calculated in nanoseconds
+    start: int = time.perf_counter_ns()
     try:
+        # yield control to the caller
         yield
     finally:
-        elapsed: float = time.perf_counter() - start
-        operation_name: str = operation_name or ""
-        if log:
-            log.debug(f"{operation_name} executed in {elapsed:.3f} seconds.")
+        # the elapsed time
+        elapsed_ns: int = time.perf_counter_ns() - start
+
+        # convert to microseconds
+        elapsed_ms = elapsed_ns / 1_000
+
+        # the variation
+        elapsed_human = humanize.precisedelta(
+            value=datetime.timedelta(microseconds=elapsed_ms),
+            minimum_unit="microseconds",
+            format="%.2f",
+        )
+
+        # if no logger is provided, use the default logger
+        if log is None:
+            log = logging.getLogger(__name__)
+
+        # if no operation name is provided, use the default
+        if operation_name is None:
+            log.debug(f"* executed in {elapsed_human}.")
+        else:
+            log.debug(f"* {operation_name} executed in {elapsed_human}.")
